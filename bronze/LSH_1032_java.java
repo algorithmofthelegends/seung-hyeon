04e1911d45e2a3d4effc5064f000cb2cd2db9df5
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2022-07-23 23:21:39 +0900

    브론즈 1032

--- bronze/LSH_1032_java.java
+++ bronze/LSH_1032_java.java
@@ -1,25 +1,23 @@
+package bronze;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class LSH_1032_java {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
         /**
-            input = 반복수
-            문자열 a,b 2개생성
-            a문자열에 첫번째 값저장
-            if input == 1 return a문자열
-            else
-            for(i =o i<input-1 i++)
-            b문자열 입력
-            for(j =o j <input길이 j++)
-            만약 a[j] 랑 b[j]가 다르다면 a[j]에 ? 입력
-            return 반환
+         input = 반복수
+         문자열 a,b 2개생성
+         a문자열에 첫번째 값저장
+         if input == 1 return a문자열
+         else
+         for(i =o i<input-1 i++)
+         b문자열 입력
+         for(j =o j <input길이 j++)
+         만약 a[j] 랑 b[j]가 다르다면 a[j]에 ? 입력
+         return 반환
          */
 
         String[] a;
@@ -56,6 +54,4 @@ public class Main {
         }
         System.out.println(answer);
     }
-
 }
-
